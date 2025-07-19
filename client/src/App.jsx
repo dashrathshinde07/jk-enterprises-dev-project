fd98baa962c0e4e35f538e8aff1b41f94d7196fe
dashrathshinde07 <dashrathbshinde@gmail.com>
2025-07-19 11:35:09 +0530

    feat: setup React frontend with basic file structure in client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <div>
+        {/* navbar */}
+        <Navbar />
+
+        {/* other components */}
+      </div>
+    </>
+  );
+}
+
+export default App;

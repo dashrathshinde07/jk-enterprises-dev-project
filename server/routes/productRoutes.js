e3c7dafa1c2d443107d9a6bfafa160935bb5204b
surwaseonkar1512 <surwaseonkar12345@gmail.com>
2025-07-22 23:34:17 +0530

    push

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -7,7 +7,7 @@ router.post("/", upload.array("images"), productController.createProduct);
 router.put("/:id", upload.array("images"), productController.updateProduct);
 router.patch("/archive/:id", productController.softDeleteProduct);
 router.get("/", productController.getAllProducts);
-router.get("/:id", productController.getProductById);
 router.get("/filter", productController.filterProducts);
+router.get("/:id", productController.getProductById);
 
 module.exports = router;
